550596ee5ba11f113c47750e2ed37184b8a09cc4
sunilyadav489 <sunilyadav489@gmail.com>
2023-03-08 16:07:13 +0530

    adding feature components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-import './App.css';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import Cart from './components/Cart';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h2>Learning Redux Toolkit</h2>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Navbar/>
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<Home/>}></Route>
+          <Route path="/cart" element={<Cart/>}></Route>
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
